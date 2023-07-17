dabc68853df9d72395cd9b69469841b2029f4b05
chwangmin <ckm7907cb@gmail.com>
2023-07-17 16:10:08 +0900

    Refactor : spring이 실행과 동시에 종료되는 부분 수정 #7

--- src/test/java/baby/lignin/LigninApplicationTests.java
+++ src/test/java/baby/lignin/LigninApplicationTests.java
@@ -1,13 +1,13 @@
-package baby.lignin;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-class LigninApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package baby.lignin;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class LigninApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
