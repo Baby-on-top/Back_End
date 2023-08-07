fea1c3ec5b0df0d14596d68b7010bc4db10fd507
wnstn819 <wnstn819@naver.com>
2023-08-08 01:20:12 +0900

    Chore : invite 경로 재수정 #18

--- src/main/java/baby/lignin/workspace/controller/WorkspaceController.java
+++ src/main/java/baby/lignin/workspace/controller/WorkspaceController.java
@@ -92,7 +92,7 @@ public class WorkspaceController {
                 .maxAge(5 * 60)
                 .build();
         response.addHeader("Set-Cookie", cookie.toString());
-        return "redirect:http://gg.lignin.today/invite-check";
+        return "redirect:http://lignin.today/invite-check";
     }
 
     @Operation(summary = "WorkSpace 초대", description = "WorkSpace 초대")
